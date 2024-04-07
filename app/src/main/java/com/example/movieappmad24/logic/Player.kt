837fcd21a7be2b7ab02158e0e4f6f9c068c083b2
Roman Schuller <roman.schuller@gmail.com>
2024-04-07 20:03:40 +0200

    erste lifecycle Implementierung

--- app/src/main/java/com/example/movieappmad24/logic/Player.kt
+++ app/src/main/java/com/example/movieappmad24/logic/Player.kt
@@ -3,10 +3,14 @@ package com.example.movieappmad24.logic
 import android.net.Uri
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
 
@@ -18,7 +22,18 @@ fun player(uri: String): ExoPlayer {
     val mediaItem = MediaItem.fromUri(uri)
     val player = remember { ExoPlayer.Builder(context).build().apply { setMediaItem(mediaItem)
         prepare() } }
+    val lifecycle = LocalLifecycleOwner.current.lifecycle
 
+    LaunchedEffect(player) {
+        val lifecycleObserver = LifecycleEventObserver { _, event ->
+            when (event) {
+                Lifecycle.Event.ON_RESUME -> player.play()
+                Lifecycle.Event.ON_STOP -> player.pause()
+                else -> Unit
+            }
+        }
+        lifecycle.addObserver(lifecycleObserver)
+    }
     return player
 }
 
